9a1a04e29a8d75d6f8cd39602c182ed11b03ec2f
asiawu <2120017378@qq.com>
2020-08-04 15:00:13 +0800

    mdm

---
+++ mdm/src/main/java/com/pindao/infrabase/mdm/App.java
@@ -0,0 +1,13 @@
+package com.pindao.infrabase.mdm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
