be4cbb5fd892115d822552419996db844123e04c
zhagnshun <zs888888>
2022-04-02 18:27:56 +0800

    测试提交

--- demo-mybatis/src/main/java/com/example/demomybatis/service/impl/OrderServiceImpl.java
+++ demo-mybatis/src/main/java/com/example/demomybatis/service/impl/OrderServiceImpl.java
@@ -5,6 +5,12 @@ import com.example.demomybatis.entity.Order;
 import com.example.demomybatis.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * desc:
@@ -16,12 +22,29 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements IOrderService {
 
 
-    @Autowired
+    @Autowired(required = false)
     OrderMapper orderMapper;
 
 
+    @Autowired
+    TransactionTemplate transactionTemplate ;
+
+
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Integer saveOrder(Order order) {
-        return orderMapper.insert(order);
+
+        transactionTemplate.execute(
+                new TransactionCallbackWithoutResult() {
+                    @Override
+                    protected void doInTransactionWithoutResult(TransactionStatus status) {
+                        orderMapper.insert(order);
+                        int i = 1/0 ;
+                    }
+                }
+        );
+
+
+        return null;
     }
 }
